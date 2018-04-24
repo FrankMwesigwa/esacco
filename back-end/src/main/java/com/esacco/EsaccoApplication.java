6004c53a275bb77fb8158494dc68f374dfd4135b
Frank Mwesigwa <frank.mwesigwa1@gmail.com>
2018-04-24 21:00:55 +0300

    bootstrapped react and springboot

---
+++ back-end/src/main/java/com/esacco/EsaccoApplication.java
@@ -0,0 +1,12 @@
+package com.esacco;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EsaccoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EsaccoApplication.class, args);
+	}
+}
